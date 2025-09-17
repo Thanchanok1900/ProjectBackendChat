a511732a9e5188d8a89b9d02da02d9e510d6f3a6
Warissara-05 <196065122+Warissara-05@users.noreply.github.com>
2025-09-17 20:25:21 +0700

    Update Database

--- config/database.js
+++ config/database.js
@@ -1,31 +1,34 @@
-const { Sequelize } = require('sequelize'); 
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  'DatabaseTranslateChat', 
-  'postgres',
-  'Rsv19390', 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: 'localhost', 
-    dialect: 'postgres'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    logging: console.log, 
   }
 );
 
 async function connect() {
-    try{
-        await sequelize.authenticate();
-        console.log('Database connected successfully.');
-    }catch(error){
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log('Database connected successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
 }
 
 async function sync() {
-    try{
-        await sequelize.sync({ alter: true });
-        console.log("Database synced successfully.");
-    }catch(error){
-        console.error('Unable to sync the database:', error);
-    }
+  try {
+    await sequelize.sync({ alter: true });
+    console.log("Database synced successfully.");
+  } catch (error) {
+    console.error('Unable to sync the database:', error);
+  }
 }
 
-module.exports = {sequelize, connect, sync};
+module.exports = { sequelize, connect, sync };
